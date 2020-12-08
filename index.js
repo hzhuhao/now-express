36ce18cb6a1acaf16d70ad681a9078a2b9307c34
huhao <hzhuhao@163.com>
2020-12-08 15:00:46 +0800

    fix

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const app = express();
 
 const port = 5000;
 
-const bot = new TelegramBot(token, {
-  polling: true
-});
+const bot = new TelegramBot(token);
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   // send a message to the chat acknowledging receipt of their message
