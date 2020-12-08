14be97445ebb1a06f7b657ba31cb1673d298f477
unknown <hzhuhao@163.com>
2020-12-08 23:29:01 +0800

    fix

--- index.js
+++ index.js
@@ -26,10 +26,16 @@ app.post("/user", (req, res) => {
   res.send({ status: "User created", name, location });
 });
 
-const token = '1477338873:AAGUTNjDJl3fsVN-jV0NBpHwHPsmBdCJqMU';
+const token = 'AAFBRC15eipigQmRUsRcDwGUKQCYep_Nrkc';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token,{polling:true});
+bot.onText(/\/start/, msg => {
+
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Welcome');
+});
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   // send a message to the chat acknowledging receipt of their message
